44ddb24202151a2648d3c9af429c4adfd96658d2
Dale <Danntabachoyy@gmail.com>
2023-09-14 17:06:31 +0800

    dynamically change bg-color base on currID

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,7 +1,10 @@
 /* eslint-disable react/prop-types */
 import NumberQuestion from "./NumberQuestion";
+import { useState } from "react";
 
 function Game({ configureQuiz, questions }) {
+  const q = questions.find((q, i) => i === 0);
+  const [currentId, setCurrentId] = useState(q.id);
   return (
     <div className="h-full flex flex-col">
       <div className="p-4">
@@ -12,7 +15,15 @@ function Game({ configureQuiz, questions }) {
       <div className="flex-1 bg-white rounded-t-3xl">
         <ul className="flex justify-evenly my-8">
           {questions.map((question, index) => {
-            return <NumberQuestion key={question.id} index={index + 1} />;
+            return (
+              <NumberQuestion
+                key={question.id}
+                id={question.id}
+                setCurrentId={setCurrentId}
+                active={question.id === currentId}
+                index={index + 1}
+              />
+            );
           })}
         </ul>
       </div>
--- src/components/NumberQuestion.jsx
+++ src/components/NumberQuestion.jsx
@@ -1,8 +1,16 @@
 /* eslint-disable react/prop-types */
-function NumberQuestion({ index }) {
+function NumberQuestion({ index, active, setCurrentId, id }) {
   return (
     <li>
-      <a href="#" className="py-3 px-4 rounded-full bg-indigo-500 text-white">
+      <a
+        href="#"
+        className={`py-3 px-4 rounded-full ${
+          active ? "bg-indigo-500" : " bg-gray-500 hover:bg-indigo-500"
+        } text-white`}
+        onClick={() => {
+          setCurrentId(id);
+        }}
+      >
         {index}
       </a>
     </li>
