ecb18b3b8e0156f571db5e0f33052b9d2b62fac1
Dale <Danntabachoyy@gmail.com>
2023-09-16 16:54:02 +0800

    u: the question are now showing

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,13 +1,24 @@
-import { useEffect } from "react";
 import possibleAnswers from "../utils/possibleAnswers";
-
 /* eslint-disable react/prop-types */
 function Question({ currentQuestion }) {
-  
+  const choices = possibleAnswers(currentQuestion);
+
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className=" space-y-4 mt-10 p-4">
+      <p>Question: {currentQuestion.question.text}</p>
+      <ul className="space-y-4">
+        {choices.map((choice, index) => {
+          return (
+            <li key={index} className="flex space-x-4 items-center cursor-pointer">
+              <span className="py-2 px-4 bg-gray-400 text-white rounded-full">
+                {choice.letter}
+              </span>
+              <p>{choice.choice}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
 
--- src/utils/possibleAnswers.jsx
+++ src/utils/possibleAnswers.jsx
@@ -3,18 +3,19 @@ function possibleAnswers(currentQuestion) {
     ...currentQuestion.incorrectAnswers,
     currentQuestion.correctAnswer,
   ];
+  const letters = [ 'A', 'B', 'C', 'D'];
   let answers = [];
   let usedIndexes = [];
   let i = 0;
   while (i < tempAns.length) {
     let randomNum = Math.floor(Math.random() * tempAns.length);
     if (!usedIndexes.includes(randomNum)) {
-      answers.push(tempAns[randomNum]);
+      answers.push({letter:letters[i], choice: tempAns[randomNum]});
       usedIndexes.push(randomNum);
       i++;
     }
   }
-  console.log(answers);
+  return answers;
 }
 
 export default possibleAnswers;
